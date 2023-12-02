2b44af1d03b2e69454b22c726959afebd80c470c
adunn39 <adunn@gatech.edu>
2023-12-02 17:53:25 -0500

    more updates to try to get pages working

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cs6440-final-project/",
   plugins: [react()],
+  base: "/cs6440-final-project/",
 })
 
 
